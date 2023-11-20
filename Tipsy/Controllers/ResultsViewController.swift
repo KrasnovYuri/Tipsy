7fed1ab72c301f4550b8fc123632d4216d460942
KrasnovYuri <krasnov1yuri@gmail.com>
2023-11-20 21:37:10 +0300

    add IBOutlets and IBActions

---
+++ Tipsy/Controllers/ResultsViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ResultsViewController.swift
+//  Tipsy
+//
+//  Created by Юрий on 20.11.2023.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var settingsLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func recalculatePressed(_ sender: Any) {
+    }
+    
+}

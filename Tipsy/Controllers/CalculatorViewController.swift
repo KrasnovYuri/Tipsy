b5a3ab274e110ac9f2dd2ea49e893d53c4bcbc83
KrasnovYuri <krasnov1yuri@gmail.com>
2023-11-21 20:26:58 +0300

    final

--- Tipsy/Controllers/CalculatorViewController.swift
+++ Tipsy/Controllers/CalculatorViewController.swift
@@ -8,21 +8,47 @@
 import UIKit
 
 class CalculatorViewController: UIViewController {
-
+    
     @IBOutlet weak var billTextField: UITextField!
     @IBOutlet weak var zeroPctButton: UIButton!
     @IBOutlet weak var tenPctButton: UIButton!
     @IBOutlet weak var twentyPctButton: UIButton!
     @IBOutlet weak var splitNumberLabel: UILabel!
     
+    var tipPercent = 0.10
+    var numberOfPeople = 2
+    var result = 1.0
+    
     @IBAction func tipChanged(_ sender: UIButton) {
+        
+        billTextField.endEditing(true)
+        
+        [zeroPctButton, tenPctButton, twentyPctButton].forEach {
+            $0.isSelected = false
+        }
+        sender.isSelected = true
+        
+        guard let buttonTitle = sender.currentTitle else { return }
+        guard let buttonTitleAsANumber = Double(buttonTitle.dropLast()) else { return }
+        tipPercent = buttonTitleAsANumber / 100
     }
     
     @IBAction func stepperValueChanged(_ sender: UIStepper) {
+        splitNumberLabel.text = String(format: "%2.f", sender.value)
+        numberOfPeople = Int(sender.value)
     }
     
     @IBAction func calculatePressed(_ sender: UIButton) {
+        guard let doubleBill = Double(billTextField.text ?? "") else { return }
+        result = doubleBill * (1 + tipPercent) / Double(numberOfPeople)
     }
     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let resultVC = segue.destination as? ResultsViewController else { return }
+        resultVC.numberOfPeople = numberOfPeople
+        resultVC.result = result
+        resultVC.tipPercent = Int(tipPercent * 100)
+    }
 }
+
 
--- Tipsy/Controllers/ResultsViewController.swift
+++ Tipsy/Controllers/ResultsViewController.swift
@@ -10,12 +10,20 @@ import UIKit
 class ResultsViewController: UIViewController {
     @IBOutlet weak var totalLabel: UILabel!
     @IBOutlet weak var settingsLabel: UILabel!
+    
+    var tipPercent = 10
+    var numberOfPeople = 2
+    var result: Double!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        totalLabel.text = String(format: "%.2f", result)
+        settingsLabel.text = "Split between \(numberOfPeople) people, with \(tipPercent)% tip."
     }
     
     @IBAction func recalculatePressed(_ sender: Any) {
+        dismiss(animated: true)
     }
     
 }
